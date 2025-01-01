096454d76d1b15cb06e9152bc504172b697e8092
sheikh-hammad <hammadusman001@gmail.com>
2025-01-01 10:49:08 +0500

    make some changes

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -2,7 +2,7 @@ import { createClient } from 'next-sanity'
 
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: 'ged7ghur',
   dataset: "production",
   apiVersion: "2024-12-29",
   token: process.env.NEXT_SANITY_TOKEN,
